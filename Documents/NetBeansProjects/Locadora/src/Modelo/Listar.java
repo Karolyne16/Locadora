18d2df72820ce2d318f629ce921bc92b2d6c2d53
Karolyne16 <karolynepontes15@gmail.com>
2019-12-05 17:45:46 -0200

    Criei o método Listar

--- Documents/NetBeansProjects/Locadora/src/Modelo/Listar.java
+++ Documents/NetBeansProjects/Locadora/src/Modelo/Listar.java
@@ -5,11 +5,36 @@
  */
 package Modelo;
 
+import java.sql.PreparedStatement;
+
 /**
  *
  * @author Aluno
  */
-public class Listar {
-    String sql = "select idcliente, nome,rg,cp,telefone,emial from cliente";
-    List 
-}
+public class Listar { 
+  public List <Cliente> ListarCliente ()  {
+    String sql = "select idcliente, nome,rg,cpf,telefone,email from cliente";
+    List <Cliente> lista = new ArrayList<>();
+     try {
+        PreparedStatement ps = getCon().prepareStatement (sql);
+        ResultSet rs = ps.executeQuery();
+        
+        if (rs !=null) {
+             while (rs.next()){
+                Cliente a = new Cliente();
+                a.setCodigo (rs.getInt(1));
+                a.setNome (rs.getString(2));
+                a.setRG (rs.getString(3));
+                a.setCPF(rs.getString(4));
+                a.setTelefone(rs.getString(5));
+                a.setEmail(rs.getString(6));
+                
+                lista.add(a);
+        }
+        return lista;
+    }else{
+       return null;    
+}catch  (SQLExcepition e)  {
+        return null;
+        }
+     }
